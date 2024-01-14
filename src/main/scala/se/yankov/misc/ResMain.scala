de3ef2623fef24d9d6e4d75713e8a66898e12168
lachezar <lucho870601@gmail.com>
2024-01-14 23:20:09 +0100

    constrain on transitive dependencies using type classes

--- src/main/scala/se/yankov/misc/ResMain.scala
+++ src/main/scala/se/yankov/misc/ResMain.scala
@@ -1,5 +1,6 @@
 package se.yankov.misc
 
+import cats.Show
 import cats.effect.std.Console
 import cats.effect.{Async, IO, IOApp, Resource}
 import cats.syntax.all.*
@@ -7,10 +8,14 @@ import cats.syntax.all.*
 object ResMain extends IOApp.Simple {
 
   trait Res0[F[_]]
-  trait Res1[F[_]]
+  trait Res1[F[_]: Res0]
   trait Res2[F[_]]
 
-  final case class Res1Impl[F[_]](res0: Res0[F]) extends Res1[F]
+  given [F[_]]: Show[Res1[F]] with
+    override def show(t: Res1[F]): String = "Res1"
+
+  given [F[_]]: Show[Res2[F]] with
+    override def show(t: Res2[F]): String = "Res2"
 
   trait App[F[_]]:
     def run: F[Nothing]
@@ -18,11 +23,11 @@ object ResMain extends IOApp.Simple {
   object App:
     def make[F[_]: Res1: Res2: Console: Async]: App[F] = new App[F]:
       override def run: F[Nothing] =
-        Console[F].println(s"hello with ${summon[Res1[F]]} and ${summon[Res2[F]]}") >> Async[F].never[Nothing]
+        Console[F].println(s"hello with ${summon[Res1[F]].show} and ${summon[Res2[F]].show}") >> Async[F].never[Nothing]
 
   def app[F[_]: Console: Async]: Resource[F, App[F]] = for {
     res0 <- Resource.pure(new Res0[F] {})
-    res1 <- Resource.pure(Res1Impl[F](res0))
+    res1 <- Resource.pure(new Res1[F](using res0) {})
     res2 <- Resource.pure(new Res2[F] {})
   } yield
     App.make[F](using res1, res2, Console[F], Async[F])
